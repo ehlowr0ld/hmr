e1b2dc9306bd7c953d269cded29e9cc3aa7419b9
Muspi Merol <me@promplate.dev>
2025-01-12 19:20:24 +0800

    remove uvicorn boilerplate by implementing a uvicorn-hmr cli

--- examples/fastapi/main.py
+++ examples/fastapi/main.py
@@ -12,35 +12,3 @@ app.include_router(b.router)
 @app.get("/")
 def redirect_to_docs():
     return RedirectResponse("/docs")
-
-
-def start_server():
-    global stop
-
-    from atexit import register, unregister
-    from threading import Event, Thread
-
-    from uvicorn import Config, Server
-
-    if stop := globals().get("stop"):  # type: ignore
-        unregister(stop)
-        stop()  # type: ignore
-
-    server = Server(Config(app, host="localhost"))
-
-    finish = Event()
-
-    def run_server():
-        server.run()
-        finish.set()
-
-    Thread(target=run_server, daemon=True).start()
-
-    @register
-    def stop():
-        server.should_exit = True
-        finish.wait()
-
-
-if __name__ == "__main__":
-    start_server()
