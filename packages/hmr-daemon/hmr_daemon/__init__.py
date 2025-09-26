3e002529baf54860817888d1c2f69b8de14aafdf
Muspi Merol <me@promplate.dev>
2025-09-26 19:17:23 +0800

    fix: ensure hmr-daemon runs as singleton by checking existing threads

--- packages/hmr-daemon/hmr_daemon/__init__.py
+++ packages/hmr-daemon/hmr_daemon/__init__.py
@@ -1,7 +1,10 @@
 import os
 
 if "NO_HMR_DAEMON" not in os.environ:
-    if os.name == "nt":
-        from . import windows  # noqa: F401
-    else:
-        from . import posix  # noqa: F401
+    from threading import enumerate
+
+    if any(t.name == "hmr-daemon" for t in enumerate()):
+        if os.name == "nt":
+            from . import windows  # noqa: F401
+        else:
+            from . import posix  # noqa: F401
