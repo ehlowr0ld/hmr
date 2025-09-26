b4fe2c4894d1b10898e2188ce68d078c91901d21
Muspi Merol <me@promplate.dev>
2025-09-27 06:59:33 +0800

    fix: correct hmr-daemon singleton logic

--- packages/hmr-daemon/hmr_daemon/__init__.py
+++ packages/hmr-daemon/hmr_daemon/__init__.py
@@ -3,7 +3,7 @@ import os
 if "NO_HMR_DAEMON" not in os.environ:
     from threading import enumerate
 
-    if any(t.name == "hmr-daemon" for t in enumerate()):
+    if all(t.name != "hmr-daemon" for t in enumerate()):
         if os.name == "nt":
             from . import windows  # noqa: F401
         else:
