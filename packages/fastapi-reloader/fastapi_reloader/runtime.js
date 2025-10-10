863d35ce6ea00fd71b8ace8b6b873d93b45565dc
Muspi Merol <me@promplate.dev>
2025-10-11 02:58:46 +0800

    chore: extract polling logic into a separate function for clarity

--- packages/fastapi-reloader/fastapi_reloader/runtime.js
+++ packages/fastapi-reloader/fastapi_reloader/runtime.js
@@ -1,4 +1,17 @@
-(async function () {
+async function poll() {
+  while (true) {
+    try {
+      const res = await fetch("/---fastapi-reloader---", { method: "HEAD" });
+      if (res.ok) {
+        break;
+      } else if (res.status !== 502) {
+        return;
+      }
+    } catch (error) {}
+  }
+}
+
+async function main() {
   const response = await fetch("/---fastapi-reloader---/0");
   const reader = response.body.getReader();
   const decoder = new TextDecoder();
@@ -10,20 +23,11 @@
     if (value) {
       const chunk = decoder.decode(value, { stream: true });
       if (chunk.includes("1")) {
-        while (true) {
-          try {
-            const res = await fetch("/---fastapi-reloader---", {
-              method: "HEAD",
-            });
-            if (res.ok) {
-              break;
-            } else if (res.status !== 502) {
-              return;
-            }
-          } catch (error) {}
-        }
+        await poll();
         location.reload();
       }
     }
   }
-})();
+}
+
+main();
