489e884c9039a556232c775dd197ef3ca5a25187
Muspi Merol <me@promplate.dev>
2025-10-11 10:37:47 +0800

    chore: micro-optimization code readability improvement

--- packages/fastapi-reloader/fastapi_reloader/patcher.py
+++ packages/fastapi-reloader/fastapi_reloader/patcher.py
@@ -14,8 +14,31 @@ from .core import reload_router
 
 
 def is_streaming_response(response: Response) -> TypeGuard[StreamingResponse]:
-    # In fact, it may not be a fastapi's StreamingResponse, but a starlette's one with the same interface
+    # In fact, it may not be a Starlette's StreamingResponse, but an internal one with the same interface
     return hasattr(response, "body_iterator")
+
+
+async def _injection_http_middleware(request: Request, call_next: Callable[[Request], Awaitable[Response]]):
+    res = await call_next(request)
+
+    if request.method != "GET" or "html" not in (res.headers.get("content-type", "")) or res.headers.get("content-encoding", "identity") != "identity":
+        return res
+
+    async def response():
+        if is_streaming_response(res):
+            async for chunk in res.body_iterator:
+                yield chunk
+        else:
+            yield res.body
+
+        yield b'\n\n <script src="/---fastapi-reloader---/poller.js"></script>'
+
+    headers = {k: v for k, v in res.headers.items() if k.lower() not in {"content-length", "transfer-encoding"}}
+
+    return StreamingResponse(response(), res.status_code, headers, res.media_type)
+
+
+injection_middleware = Middleware(BaseHTTPMiddleware, dispatch=_injection_http_middleware)
 
 
 def patch_for_auto_reloading(app: ASGIApp):
@@ -28,25 +51,6 @@ def patch_for_auto_reloading(app: ASGIApp):
     new_app.include_router(reload_router)
     new_app.mount("/", app)
 
-    async def hmr_middleware(request: Request, call_next: Callable[[Request], Awaitable[Response]]):
-        res = await call_next(request)
-
-        if request.method != "GET" or "html" not in (res.headers.get("content-type", "")) or res.headers.get("content-encoding", "identity") != "identity":
-            return res
-
-        async def response():
-            if is_streaming_response(res):
-                async for chunk in res.body_iterator:
-                    yield chunk
-            else:
-                yield res.body
-
-            yield b'\n\n <script src="/---fastapi-reloader---/poller.js"></script>'
-
-        headers = {k: v for k, v in res.headers.items() if k.lower() not in {"content-length", "transfer-encoding"}}
-
-        return StreamingResponse(response(), res.status_code, headers, res.media_type)
-
-    new_app.user_middleware.append(Middleware(BaseHTTPMiddleware, dispatch=hmr_middleware))  # the last middleware is the first one to be called
+    new_app.user_middleware.append(injection_middleware)  # the last middleware is the first one to be called
 
     return new_app
